99ef9d0b39bd735ecb284d7b6feb2cf2244bf4df
Hamidputrawan33 <terhuracloth@gmail.com>
2022-09-30 10:30:56 +0700

    menambahkan

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+Class contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>"
+    echo "Nama saya Hamid Putrawan";
+                 
+
+  }
+
+
+}
